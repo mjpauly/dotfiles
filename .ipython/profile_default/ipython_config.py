35d7b44f5df89bd20517bb7adeeffc73b5efa09e
Matthew Pauly <mjpauly@mac.com>
2017-09-25 11:29:09 -0700

    remove unused autoload commands

--- .ipython/profile_default/ipython_config.py
+++ .ipython/profile_default/ipython_config.py
@@ -2,7 +2,5 @@ c.InteractiveShellApp.extensions = ['autoreload']
 c.InteractiveShellApp.exec_lines = [
     '%autoreload 1',
     'from datetime import datetime, timedelta',
-    'import imp',
-    'fetch = imp.load_source("fetch", "/sanhome/yshah/Util/fetch.py")'
 ]
 c.InteractiveShellApp.pylab='auto'
